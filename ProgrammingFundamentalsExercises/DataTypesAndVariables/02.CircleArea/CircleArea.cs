0f464d0c73846d42a98c1aa752c30914c3692ec7
TheXaXo <thexaxo90@gmail.com>
2016-09-22 17:10:59 +0300

    Add new exercise

---
+++ ProgrammingFundamentalsExercises/DataTypesAndVariables/02.CircleArea/CircleArea.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class CircleArea
+{
+    static void Main(string[] args)
+    {
+        double r = double.Parse(Console.ReadLine());
+        double circleArea = Math.PI * Math.Pow(r, 2);
+        Console.WriteLine("{0:f12}", circleArea);
+    }
+}
