75df7b500684eeeb7e1b60a403d1470a4f644fd3
TheXaXo <thexaxo90@gmail.com>
2016-09-30 12:13:00 +0300

    Add exercises.

--- ProgrammingFundamentalsExercises/DataTypesAndVariablesExercises/10.CenturiesToNanoseconds/CenturiesToNanoseconds.cs
+++ ProgrammingFundamentalsExercises/DataTypesAndVariablesExercises/10.CenturiesToNanoseconds/CenturiesToNanoseconds.cs
@@ -6,7 +6,7 @@ class CenturiesToNanoseconds
     {
         int centuries = int.Parse(Console.ReadLine());
         int years = centuries * 100;
-        uint days = (uint) (years * 365.2422);
+        uint days = (uint)(years * 365.2422);
         ulong hours = days * 24;
         ulong minutes = hours * 60;
         ulong seconds = minutes * 60;
@@ -14,6 +14,6 @@ class CenturiesToNanoseconds
         decimal microseconds = milliseconds * 1000;
         decimal nanoseconds = microseconds * 1000;
 
-        Console.WriteLine("{0} centuries = {1} years = {2} days = {3} hours = {4} minutes = {5} seconds = {6} milliseconds = {7} microseconds = {8} nanoseconds", centuries, years, days, hours, minutes, seconds, milliseconds, microseconds, nanoseconds); 
+        Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = {seconds} seconds = {milliseconds} milliseconds = {microseconds} microseconds = {nanoseconds} nanoseconds");
     }
 }
---
+++ ProgrammingFundamentalsExercises/MethodsAndDebuggingExercises/15.InstructionSet/InstructionSet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class InstructionSet
+{
+    static void Main()
+    {
+        string command = Console.ReadLine();
+
+        while (command != "END")
+        {
+            string[] split = command.Split(' ');
+            long result = 0;
+            switch (split[0])
+            {
+                case "INC":
+                    {
+                        int operandOne = int.Parse(split[1]);
+                        result = (long)operandOne + 1;
+                        break;
+                    }
+                case "DEC":
+                    {
+                        int operandOne = int.Parse(split[1]);
+                        result = (long)operandOne - 1;
+                        break;
+                    }
+                case "ADD":
+                    {
+                        int operandOne = int.Parse(split[1]);
+                        int operandTwo = int.Parse(split[2]);
+                        result = (long)operandOne + operandTwo;
+                        break;
+                    }
+                case "MLA":
+                    {
+                        int operandOne = int.Parse(split[1]);
+                        int operandTwo = int.Parse(split[2]);
+                        result = (long)operandOne * operandTwo;
+                        break;
+                    }
+            }
+            Console.WriteLine(result);
+            command = Console.ReadLine();
+        }
+    }
+}
