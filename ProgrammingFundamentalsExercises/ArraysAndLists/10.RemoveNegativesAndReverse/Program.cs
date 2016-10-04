688fbb9a067fe90d863edfe7d5cec1871111c0a4
TheXaXo <thexaxo90@gmail.com>
2016-10-04 09:54:37 +0300

    Add exercises.

---
+++ ProgrammingFundamentalsExercises/ArraysAndLists/10.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = GetIntArray(Console.ReadLine());
+        List<int> fixedArray = new List<int>();
+        bool hasItems = false;
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            if (array[i] >= 0)
+            {
+                fixedArray.Add(array[i]);
+                hasItems = true;
+            }
+        }
+
+        if(!hasItems)
+        {
+            Console.WriteLine("empty");
+        }
+        else
+        {
+            Console.WriteLine(string.Join(" ", fixedArray));
+        }
+    }
+
+    static int[] GetIntArray(string input)
+    {
+        string[] arrayAsString = input.Split(' ');
+        int[] intArray = new int[arrayAsString.Length];
+
+        for (int i = 0; i < arrayAsString.Length; i++)
+        {
+            intArray[i] = int.Parse(arrayAsString[i]);
+        }
+        return intArray;
+    }
+}
---
+++ ProgrammingFundamentalsExercises/ArraysAndLists/11.AppendLists/AppendLists.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class AppendLists
+{
+    static void Main(string[] args)
+    {
+        string[] split = Console.ReadLine().Split('|');
+        List<int> result = new List<int>();
+
+        for (int i = split.Length - 1; i >= 0; i--)
+        {
+            string[] element = split[i].Split(' ');
+            for (int j = 0; j < element.Length; j++)
+            {
+                if (element[j] != "")
+                {
+                    result.Add(int.Parse(element[j]));
+                }
+            }
+        }
+
+        foreach (int element in result)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
