92227ad00441823da99f7862b8fd914c2f12fb63
TheXaXo <thexaxo90@gmail.com>
2016-12-06 21:47:45 +0200

    Add ASP.NET Blog basic functionality.

---
+++ SoftwareTechnologies/ASP.NET Blog/ASP.NET Blog/Startup.cs
@@ -0,0 +1,20 @@
+﻿using ASP.NET_Blog.Migrations;
+using ASP.NET_Blog.Models;
+using Microsoft.Owin;
+using Owin;
+using System.Data.Entity;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_Blog.Startup))]
+namespace ASP.NET_Blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<BlogDbContext, Configuration>());
+
+            ConfigureAuth(app);
+        }
+    }
+}
