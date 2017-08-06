2ce80530db8eb101c2b89e94bbfab24736f16ae2
TheXaXo <thexaxo90@gmail.com>
2017-08-06 14:54:48 +0300

    Add JS Apps Single Page Applications exercises part 2.

--- JS Core/JavaScript Apps/JavaScript Apps Exercises/SinglePageApplicationsExercises/scripts/prodavachnik.js
+++ JS Core/JavaScript Apps/JavaScript Apps Exercises/SinglePageApplicationsExercises/scripts/prodavachnik.js
@@ -15,26 +15,43 @@ function startApp() {
     const registerView = $("#viewRegister");
     const adsView = $("#viewAds");
     const createAdView = $("#viewCreateAd");
+    const editAdView = $("#viewEditAd");
 
     const loginFormButton = $("#buttonLoginUser");
     const registerFormButton = $("#buttonRegisterUser");
+    const createAdFormButton = $("#buttonCreateAd");
+    const editAdFormButton = $("#buttonEditAd");
+
+    const loadingBox = $("#loadingBox");
+    const errorBox = $("#errorBox");
 
     homeButton.on("click", () => showView("home"));
     loginButton.on("click", () => showView("login"));
     registerButton.on("click", () => showView("register"));
-    listAdsButton.on("click", () => showView("ads"));
+    listAdsButton.on("click", listAdvertisements);
     createAdButton.on("click", () => showView("createAd"));
 
     registerFormButton.on("click", registerUser);
     loginFormButton.on("click", loginUser);
     logoutButton.on("click", logoutUser);
+    createAdFormButton.on("click", createAd);
 
     renderMenuItems();
     showView("home");
 
+    $(document).on({
+        ajaxStart: () => loadingBox.show(),
+        ajaxStop: () => loadingBox.fadeOut()
+    });
+
+    function showError(data) {
+        errorBox.text(data.responseJSON.description);
+        errorBox.show();
+        setTimeout(() => errorBox.fadeOut(), 3000);
+    }
+
     function renderMenuItems() {
         let username = localStorage.username;
-        console.log(username);
 
         if (username === undefined) {
             homeButton.show();
@@ -59,6 +76,7 @@ function startApp() {
         registerView.hide();
         adsView.hide();
         createAdView.hide();
+        editAdView.hide();
 
         switch (viewName) {
             case "home":
@@ -75,6 +93,9 @@ function startApp() {
                 break;
             case "createAd":
                 createAdView.show();
+                break;
+            case "editAd":
+                editAdView.show();
                 break;
         }
     }
@@ -99,8 +120,8 @@ function startApp() {
         };
 
         $.ajax(req).then(data => {
-            login(usernameField.val(), passwordField.val());
-        });
+            login(usernameField, passwordField);
+        }).catch(showError);
     }
 
     function loginUser() {
@@ -109,13 +130,13 @@ function startApp() {
         let usernameField = loginForm.find("[name='username']");
         let passwordField = loginForm.find("[name='passwd']");
 
-        login(usernameField.val(), passwordField.val());
-
-        usernameField.val("");
-        passwordField.val("");
+        login(usernameField, passwordField);
     }
 
-    function login(username, password) {
+    function login(usernameField, passwordField) {
+        let username = usernameField.val();
+        let password = passwordField.val();
+
         let req = {
             url: `${baseUrl}/user/${appKey}/login`,
             method: "POST",
@@ -135,14 +156,178 @@ function startApp() {
 
             renderMenuItems();
             showView("home");
-        });
+
+            usernameField.val("");
+            passwordField.val("");
+        }).catch(showError);
     }
 
     function logoutUser() {
+        let req = {
+            url: `${baseUrl}/user/${appKey}/_logout`,
+            method: "POST",
+            headers: {"Authorization": `Kinvey ${localStorage.authtoken}`}
+        };
+
+        $.ajax(req).catch(showError);
+
         localStorage.removeItem("username");
         localStorage.removeItem("authtoken");
 
         renderMenuItems();
         showView("home");
+    }
+
+    function listAdvertisements() {
+        let table = $("#ads").find("table");
+        let tableChildren = table.find("tr");
+
+        for (let i = 1; i < tableChildren.length; i++) {
+            $(tableChildren[i]).detach();
+        }
+
+        let req = {
+            url: `${baseUrl}/appdata/${appKey}/ads`,
+            headers: {"Authorization": `Kinvey ${localStorage.authtoken}`}
+        };
+
+        $.ajax(req).then(data => {
+            for (let ad of data) {
+                let row = $("<tr>");
+                row.append(`<td>${ad.title}</td>`);
+                row.append(`<td>${ad.publisher}</td>`);
+                row.append(`<td>${ad.description}</td>`);
+                row.append(`<td>${ad.price}</td>`);
+                row.append(`<td>${ad.date}</td>`);
+
+                if (ad.publisher === localStorage.username) {
+                    let buttonsColumn = $("<td>");
+                    let deleteButton = $("<a href='#'>[Delete]</a>");
+                    let editButton = $("<a href='#'>[Edit]</a>");
+
+                    deleteButton.on("click", () => deleteAd(ad._id));
+                    editButton.on("click", () => editAd(ad));
+
+                    buttonsColumn.append([deleteButton, editButton]);
+                    row.append(buttonsColumn);
+                } else {
+                    row.append("<td>");
+                }
+
+                table.append(row);
+            }
+
+            showView("ads");
+        }).catch(showError);
+    }
+
+    function createAd() {
+        let createAdForm = $("#formCreateAd");
+
+        let titleField = createAdForm.find("[name='title']");
+        let descriptionField = createAdForm.find("[name='description']");
+        let datePublishedField = createAdForm.find("[name='datePublished']");
+        let priceField = createAdForm.find("[name='price']");
+
+        let title = titleField.val();
+        let description = descriptionField.val();
+        let date = datePublishedField.val();
+        let price = Number(priceField.val());
+        let publisher = localStorage.username;
+
+        let dateTokens = date.split("-");
+        let year = dateTokens[0];
+        let month = dateTokens[1];
+        let day = dateTokens[2];
+
+        date = `${month}/${day}/${year}`;
+
+        let req = {
+            url: `${baseUrl}/appdata/${appKey}/ads`,
+            method: "POST",
+            headers: {
+                "Authorization": `Kinvey ${localStorage.authtoken}`,
+                "Content-Type": "application/json"
+            },
+            data: JSON.stringify({
+                title,
+                description,
+                date,
+                price,
+                publisher
+            })
+        };
+
+        $.ajax(req).then(data => {
+            listAdvertisements();
+
+            titleField.val("");
+            descriptionField.val("");
+            datePublishedField.val("");
+            priceField.val("");
+        }).catch(showError);
+    }
+
+    function deleteAd(adId) {
+        let req = {
+            url: `${baseUrl}/appdata/${appKey}/ads/${adId}`,
+            method: "DELETE",
+            headers: {"Authorization": `Kinvey ${localStorage.authtoken}`}
+        };
+
+        $.ajax(req).then(data => {
+            listAdvertisements();
+        }).catch(showError)
+    }
+
+    function editAd(ad) {
+        let editAdForm = $("#formEditAd");
+
+        let titleField = editAdForm.find("[name='title']");
+        let descriptionField = editAdForm.find("[name='description']");
+        let dateField = editAdForm.find("[name='datePublished']");
+        let priceField = editAdForm.find("[name='price']");
+
+        let dateTokens = ad.date.split("/");
+        let month = dateTokens[0];
+        let day = dateTokens[1];
+        let year = dateTokens[2];
+
+        titleField.val(ad.title);
+        descriptionField.val(ad.description);
+        dateField.val(`${year}-${month}-${day}`);
+        priceField.val(ad.price);
+
+        showView("editAd");
+
+        editAdFormButton.off();
+        editAdFormButton.on("click", editAdOnClick);
+
+        function editAdOnClick() {
+            dateTokens = dateField.val().split("-");
+            let year = dateTokens[0];
+            let month = dateTokens[1];
+            let day = dateTokens[2];
+
+            let req = {
+                url: `${baseUrl}/appdata/${appKey}/ads/${ad._id}`,
+                method: "PUT",
+                headers: {
+                    "Authorization": `Kinvey ${localStorage.authtoken}`,
+                    "Content-Type": "application/json"
+                },
+                data: JSON.stringify({
+                    "title": titleField.val(),
+                    "description": descriptionField.val(),
+                    "date": `${month}/${day}/${year}`,
+                    "price": Number(priceField.val()),
+                    "publisher": localStorage.username
+                })
+            };
+
+            $.ajax(req).then((data) => {
+                listAdvertisements();
+            }).catch(showError);
+        }
     }
 }
