057f72dd63f8a6c487a8f816b149361656c86bf3
TheXaXo <thexaxo90@gmail.com>
2019-01-31 00:52:47 +0200

    Add B Trees and Red Black Trees.

---
+++ Open Courses/Data Structures/B Trees and Red Black Trees/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        RedBlackTree<Integer> tree = new RedBlackTree<>();
+        tree.insert(5);
+        tree.insert(10);
+        int a = 5;
+    }
+}
